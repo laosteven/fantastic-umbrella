9f5684878bfd20b247c284480b7fc84443e1285b
Steven Lao <lao.steven92@gmail.com>
2018-11-18 10:05:32 -0500

    Firebase upload

--- src/native/components/CameraPage.js
+++ src/native/components/CameraPage.js
@@ -6,13 +6,14 @@ import { Ionicons } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 import firebaseConfig from '../../constants/firebase';
 import { Firebase } from '../../lib/firebase';
+import axios from 'axios';
 
 class CameraPage extends React.Component {
     static propTypes = {
         error: PropTypes.string,
         loading: PropTypes.bool.isRequired,
     }
-
+    
     constructor(props) {
         super(props);
         this.state = {
@@ -22,6 +23,12 @@ class CameraPage extends React.Component {
 
         this.flip = this.flip.bind(this);
         this.snap = this.snap.bind(this);
+    }
+
+    async componentWillMount() {
+      await Permissions.askAsync(Permissions.CAMERA);
+      await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      this.setState({ hasCameraPermission: status === 'granted' });
     }
 
     flip = () => {
@@ -38,151 +45,139 @@ class CameraPage extends React.Component {
                 isLoading: true
             });
 
-            const options = {base64:true};
-            this.camera.takePictureAsync({metadata: options}).then(data => {
+            this.camera.takePictureAsync().then(data => {
                 console.log(data);
 
-                var data = new FormData();
-                data.append('theFile', { uri: data.uri, name: 'profile_photo.jpg', type: 'image/jpg' });
-            
-                fetch(firebaseConfig.apiUrl + firebaseConfig.apiKey, {
-                    method: 'POST',
-                    body: data
-                })
-                    .then((response) => response.json())
-                    .then((responseJson) => {
-            
-                        alert(JSON.stringify(responseJson))
-                        alert(responseJson)
-            
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                        alert(error)
-                    });
-
-                // const metadata = { contentType: 'image/jpeg' };
-                // const task = ref.child('test').put(data.uri, metadata);
-                // task
-                //     .then(snapshot => snapshot.ref.getDownloadURL())
-                //     .then(url => console.log(url))
-                //     .catch((e) => {
-                //         console.log(e);
-                //     });
                 
-                // Image.getSize(data.uri, (width, height) => {
-                //     let imageSettings = {
-                //         offset: { x: 0, y: 0 },
-                //         size: { width: width, height: height }
-                //     };
-                //     ImageEditor.cropImage(data.uri, imageSettings, (uri) => {
-                //         ImageStore.getBase64ForTag(uri, (base64) => {
-                //             fetch(firebaseConfig.apiUrl + firebaseConfig.apiKey, {
-                //                 method: 'POST',
-                //                 headers: {
-                //                   Accept: 'application/json',
-                //                   'Content-Type': 'multipart/form-data',
-                //                 },
-                //                 body: JSON.stringify({
-                //                     "requests": [
-                //                         {
-                //                             "image": {
-                //                                 "content": 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYXVTbxmHQuOogxDeNx7U6z7neHX4kOQPZEBLG5nzQZoMMhkFN'
-                //                             },
-                //                             "features": [
-                //                                 {
-                //                                     "type":"FACE_DETECTION",
-                //                                     "maxResults": 10
-                //                                 }
-                //                             ]
-                //                         }
-                //                     ]
-                //                 })
-                //             }).done((response) => {
-                //                 console.log(response);
-                //                 // let json = response.json();
-
-                //             }, (err) => {
-                //                 console.error('promise rejected')
-                //                 console.error(err)
-                //             });
-                //         }, e => console.warn("getBase64ForTag: ", e))
-                //     }, e => console.warn("cropImage: ", e))
-                // })
-
-                // ImageStore.getBase64ForTag(data.uri, (base64) => {
-                //     console.log(base64);
-                //     fetch(firebaseConfig.apiUrl + firebaseConfig.apiKey, {
-                //         method: 'POST',
-                //         body: JSON.stringify({
-                //             "requests": [
+                // let body = {
+                //     "requests": [
+                //         {
+                //             "image": {
+                //                 "source": {
+                //                     "imageUri": "https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/03/27/09/meghan-markle-27.03.18.jpg"
+                //                 }
+                //             },
+                //             "features": [
                 //                 {
-                //                     "image": {
-                //                         "content": base64
-                //                     },
-                //                     "features": [
-                //                         {
-                //                             "type": "LABEL_DETECTION"
-                //                         }
-                //                     ]
+                //                     "type": "FACE_DETECTION",
+                //                     "maxResults": 1
                 //                 }
                 //             ]
-                //         })
-                //     }).then((response) => {
-                //         console.log(response);
-                //         return response.json();
-                //     }, (err) => {
-                //         console.error('promise rejected')
-                //         console.error(err)
-                //     });
-                // }, (reason) => console.error(reason));
-
-                // console.log(data);
-
-                // const image = {
-                //     uri: data.uri,
-                //     type: 'image/jpeg',
-                //     name: 'img' + '-' + Date.now() + '.jpg'
+                //         }
+                //     ]
                 // }
-                // const imgBody = new FormData();
-                // imgBody.append('image', image);
-                // fetch(firebaseConfig.apiUrl + firebaseConfig.apiKey, {
-                //     method: 'POST',
-                //     body: JSON.stringify({
-                //         "requests": [
-                //             {
-                //                 "image": {
-                //                     "content": data
-                //                 },
-                //                 "features": [
-                //                     {
-                //                         "type": "LABEL_DETECTION"
-                //                     }
-                //                 ]
-                //             }
-                //         ]
-                //     })
-                // }).then((response) => {
-                //     console.log(response)
-                //     return response.json();
-                // }, (err) => {
-                //     console.error('promise rejected')
-                //     console.error(err)
-                // });
+                // axios.post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAUTBKdqmia8kPKUu_1GwYtDd87F0Mwj-k', body)
+                // .then((response) => console.log(response));
+                
+                Image.getSize(data.uri, (width, height) => {
+                    let imageSettings = {
+                        offset: { x: 0, y: 0 },
+                        size: { width: width, height: height }
+                    };
+                    ImageEditor.cropImage(data.uri, imageSettings, (uri) => {
+                        ImageStore.getBase64ForTag(uri, (base64) => {
 
-                // NativeModules.RNImageToBase64.getBase64String(data.uri, (err, base64) => {
-                //     if (err) {
-                //         console.error(err)
-                //     }
-                //     console.log(base64);
 
-                //     let result = this.sendToGoogleCloud(base64);
-                //     console.log(result);
+                            var storageRef = Firebase.storage().ref();
+                            var imageRef = storageRef.child("images");
+                            var testRef = imageRef.child(new Date() + "-test.jpg");
+                            
+                            // var task = testRef.put(blob);
+                            // var task = testRef.put(new File(base64, data.uri));
+                            var task = testRef.putString(base64, 'raw')
 
-                //     this.setState({
-                //         isLoading: false
-                //     });
-                // });
+                            task.on('state_changed', function(snapshot){
+                                // Observe state change events such as progress, pause, and resume
+                                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                                console.log('Upload is ' + progress + '% done');
+                                switch (snapshot.state) {
+                                  case firebase.storage.TaskState.PAUSED: // or 'paused'
+                                    console.log('Upload is paused');
+                                    break;
+                                  case firebase.storage.TaskState.RUNNING: // or 'running'
+                                    console.log('Upload is running');
+                                    break;
+                                }
+                              }, function(error) {
+                                // Handle unsuccessful uploads
+                              }, function() {
+                                // Handle successful uploads on complete
+                                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                                task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                                  console.log('File available at', downloadURL);
+                                  
+                                    let body = {
+                                    "requests": [
+                                        {
+                                            "image": {
+                                                "source": {
+                                                "imageUri": downloadURL
+                                                }
+                                            },
+                                            "features": [
+                                                {
+                                                    "type": "FACE_DETECTION",
+                                                    "maxResults": 1
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                                axios.post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAUTBKdqmia8kPKUu_1GwYtDd87F0Mwj-k', body)
+                                .then((response) => console.log(response));
+                                });
+                            });
+
+                            // let body = {
+                            //     "requests": [
+                            //         {
+                            //             "image": {
+                            //                 "content": base64
+                            //             },
+                            //             "features": [
+                            //                 {
+                            //                     "type": "FACE_DETECTION",
+                            //                     "maxResults": 1
+                            //                 }
+                            //             ]
+                            //         }
+                            //     ]
+                            // }
+                            // axios.post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAUTBKdqmia8kPKUu_1GwYtDd87F0Mwj-k', body)
+                            // .then((response) => console.log(response));
+
+                            // fetch("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAUTBKdqmia8kPKUu_1GwYtDd87F0Mwj-k", {
+                            //     method: 'POST',
+                            //     headers: { "Content-Type": "application/json" },
+                            //     body: JSON.stringify({
+                            //         "requests": [
+                            //           {
+                            //             "image": {
+                            //               "source": {
+                            //                 "imageUri": "https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/03/27/09/meghan-markle-27.03.18.jpg"
+                            //               }
+                            //             },
+                            //             "features": [
+                            //               {
+                            //                 "type": "FACE_DETECTION",
+                            //                 "maxResults": 1
+                            //               }
+                            //             ]
+                            //           }
+                            //         ]
+                            //        })
+                            // }).done((response) => {
+                            //     console.log(response);
+                            //     // let json = response.json();
+
+                            // }, (err) => {
+                            //     console.error('promise rejected')
+                            //     console.error(err)
+                            // });
+                        }, e => console.log(e))
+                    }, e => console.log(e))
+                })
             });
 
         }
